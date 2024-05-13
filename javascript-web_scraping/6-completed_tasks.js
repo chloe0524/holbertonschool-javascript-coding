b6e0f5a4b04743846e6f6588e3021d0ed8eff416
chloe0524 <chloco31@gmail.com>
2024-05-13 17:20:08 +0200

    task 6:  script that computes the number of tasks completed by user id

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiURL = process.argv[2];
+
+request(apiURL, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const todos = JSON.parse(body);
+    const doneTodos = todos.filter(todo => todo.completed);
+    const doneTasks = {};
+
+    for (const todo of doneTodos) {
+      doneTasks[todo.userId] = (doneTasks[todo.userId] || 0) + 1;
+    }
+
+    console.log(doneTasks);
+  }
+});
