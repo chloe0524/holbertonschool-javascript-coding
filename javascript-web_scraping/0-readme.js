0b50a75b76313662a8b0a1315ac1e9a68a53c6fe
chloe0524 <chloco31@gmail.com>
2024-05-13 09:10:30 +0200

    task 0: Write a script that reads and prints the content of a file.

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const yargs = require('yargs');
+const argv = yargs.argv;
+const filePath = argv._[0];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
