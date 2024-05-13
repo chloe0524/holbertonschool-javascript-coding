c37354a6be4affc55e1b1d0f416bddc13a0b2b64
chloe0524 <chloco31@gmail.com>
2024-05-13 17:25:39 +0200

    semistandard fix

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,16 +4,14 @@ const request = require('request');
 const apiURL = process.argv[2];
 
 request(apiURL, (err, response, body) => {
-  if (err) {
-    console.error(err);
-  } 
-    const todos = JSON.parse(body);
-    const doneTodos = todos.filter(todo => todo.completed);
-    const doneTasks = {};
+  if (err) throw err;
+  const todos = JSON.parse(body);
+  const doneTodos = todos.filter(todo => todo.completed);
+  const doneTasks = {};
 
-    for (const todo of doneTodos) {
-      doneTasks[todo.userId] = (doneTasks[todo.userId] || 0) + 1;
-    }
+  for (const todo of doneTodos) {
+    doneTasks[todo.userId] = (doneTasks[todo.userId] || 0) + 1;
+  }
 
-    console.log(doneTasks);
+  console.log(doneTasks);
 });
