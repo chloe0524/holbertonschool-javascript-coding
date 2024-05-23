817f19f06173dea9c3befe2be8962588d4be7eec
chloe0524 <chloco31@gmail.com>
2024-05-23 09:07:45 +0000

    task 4: Create HTTP server on port 1245, display text, export app

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
+const expressVpn = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 }).listen(1245, () => console.log('Server running at http://localhost:1245/'));
 
-module.exports = app;
+module.exports = expressVpn;
