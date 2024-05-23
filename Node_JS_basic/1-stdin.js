a9207d5225d907100fba10c500807d1267e69859
chloe0524 <chloco31@gmail.com>
2024-05-23 08:48:13 +0000

    eslint disable

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
-/* eslint-disable */
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  readline.question('Welcome to Holberton School, what is your name?\n', name => {
-    process.stdout.write(`Your name is: ${name}`);
-    readline.close();
-  });
-  
-  readline.on('close', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const userData = process.stdin.read();
+
+  if (userData !== null) {
+    process.stdout.write(`Your name is: ${userData}`);
+  }
+});
+
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
+});
