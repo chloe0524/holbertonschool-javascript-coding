50dcba0ebcfa55342678453e8d7dab6a9573cf28
chloe0524 <chloco31@gmail.com>
2024-05-23 08:38:15 +0000

    task 1: Node.js CLI program, prompts for name, displays input, handles exit

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+const readline = require('readline').createInterface({
+    input: process.stdin, output: process.stdout
+  });
+  
+  readline.question('Welcome to Holberton School, what is your name?\n', name => {
+    console.log(`Your name is: ${name}`);
+    readline.close();
+  });
+  
+  readline.on('close', () => {
+    console.log('This important software is now closing');
+  });
