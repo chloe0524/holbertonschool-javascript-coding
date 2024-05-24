5940fd72c42201b1660f2316ccb3091def261751
chloe0524 <chloco31@gmail.com>
2024-05-24 10:31:42 +0200

    change usage of express instead of http

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,35 +1,38 @@
 /*eslint-disable*/
-const http = require('http');
+const expressus = require('express');
 const fs = require('fs');
 const readline = require('readline');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    const path = process.argv[2];
-    const rl = readline.createInterface({
-      input: fs.createReadStream(path),
-      output: process.stdout,
-      terminal: false,
-    });
+const applicatio = expressus();
+const porta = 1245;
 
-    let csCount = 0; let sweCount = 0; const csList = []; const
-      sweList = [];
-    rl.on('line', (line) => {
-      const [firstName, , , field] = line.split(',').map((item) => item.trim());
-      if (field === 'CS') { csCount++; csList.push(firstName); } else if (field === 'SWE') { sweCount++; sweList.push(firstName); }
-    });
-
-    rl.on('close', () => {
-      res.end(`This is the list of our students
-        Number of students: ${csCount + sweCount}
-        Number of students in CS: ${csCount}. List: ${csList.join(', ')}
-        Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`);
-    });
-  }
+applicatio.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 
-server.listen(1245, () => {
-  console.log('Server is running on http://localhost:1245');
+applicatio.get('/students', (req, res) => {
+  const via = process.argv[2];
+  const rl = readline.createInterface({
+    input: fs.createReadStream(via),
+    output: process.stdout,
+    terminal: false,
+  });
+
+  let numerusCS = 0; let numerusSWE = 0; const nominibusCS = []; const
+    nominibusSWE = [];
+  rl.on('line', (linea) => {
+    const [nomenPrimum, , , disciplina] = linea.split(',').map((item) => item.trim());
+    if (disciplina === 'CS') { numerusCS++; nominibusCS.push(nomenPrimum); } else if (disciplina === 'SWE') { numerusSWE++; nominibusSWE.push(nomenPrimum); }
+  });
+
+  rl.on('close', () => {
+    res.send(`This is the list of our students
+      Number of students: ${numerusCS + numerusSWE}
+      Number of students in CS: ${numerusCS}. List: ${nominibusCS.join(', ')}
+      Number of students in SWE: ${numerusSWE}. List: ${nominibusSWE.join(', ')}`);
+  });
+});
+
+applicatio.listen(porta, () => {
+  console.log(`Server is running on http://localhost:${porta}`);
 });
